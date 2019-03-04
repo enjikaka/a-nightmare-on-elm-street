01182773540d10b3c1220c40dd22a9af0f573fad
karlsson@jeremy.se <karlsson@jeremy.se>
2019-03-04 20:08:20 +0000

    Deploy from Forestry.io

--- Messages.elm
+++
@@ -1,8 +0,0 @@
-module Messages exposing (..)
-
-import Models exposing (ActualTrack)
-import Navigation exposing (Location)
-import RemoteData exposing (WebData)
-
-type Message = OnFetchTrack (WebData ActualTrack)
-             | OnLocationChange Location
